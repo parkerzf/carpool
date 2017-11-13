e076d1176294b3b311003dae64473066bb61e880
Feng Zhao <fezhao@ean.com>
2017-11-13 20:06:56 +0000

    exp 1

--- scripts/exp.sh
+++ scripts/exp.sh
@@ -1,18 +1,19 @@
 # sh scripts/exp.sh
 
 BASEDIR=$(dirname "$0")
+NUM_INSTANCE=50
 
 for NUM_USER in 40 60 80 100
 do
-	${BASEDIR}/sub1.sh exp_skew threshold_C51_0.5 ${NUM_USER} 0 50 20
-	${BASEDIR}/sub1.sh exp_skew threshold_R51_0.3 ${NUM_USER} 0 50 20
-	${BASEDIR}/sub1.sh exp_skew threshold_R51_0.3 ${NUM_USER} 1 50 20
-	${BASEDIR}/sub1.sh exp_skew threshold_R51_0.3 ${NUM_USER} 2 50 20
-	${BASEDIR}/sub1.sh exp_skew threshold_R51_0.3 ${NUM_USER} 3 50 20
+	${BASEDIR}/sub1.sh exp_skew threshold_C51_0.5 ${NUM_USER} 0 50 ${NUM_INSTANCE}
+	${BASEDIR}/sub1.sh exp_skew threshold_R51_0.3 ${NUM_USER} 0 50 ${NUM_INSTANCE}
+	${BASEDIR}/sub1.sh exp_skew threshold_R51_0.3 ${NUM_USER} 1 50 ${NUM_INSTANCE}
+	${BASEDIR}/sub1.sh exp_skew threshold_R51_0.3 ${NUM_USER} 2 50 ${NUM_INSTANCE}
+	${BASEDIR}/sub1.sh exp_skew threshold_R51_0.3 ${NUM_USER} 3 50 ${NUM_INSTANCE}
 
-	${BASEDIR}/sub1.sh exp_HTP threshold_C51_0.5 ${NUM_USER} 0 25 20
-	${BASEDIR}/sub1.sh exp_HTP threshold_R51_0.3 ${NUM_USER} 0 25 20
-	${BASEDIR}/sub1.sh exp_HTP threshold_R51_0.3 ${NUM_USER} 1 25 20
-	${BASEDIR}/sub1.sh exp_HTP threshold_R51_0.3 ${NUM_USER} 2 25 20
-	${BASEDIR}/sub1.sh exp_HTP threshold_R51_0.3 ${NUM_USER} 3 25 20
+	${BASEDIR}/sub1.sh exp_HTP threshold_C51_0.5 ${NUM_USER} 0 25 ${NUM_INSTANCE}
+	${BASEDIR}/sub1.sh exp_HTP threshold_R51_0.3 ${NUM_USER} 0 25 ${NUM_INSTANCE}
+	${BASEDIR}/sub1.sh exp_HTP threshold_R51_0.3 ${NUM_USER} 1 25 ${NUM_INSTANCE}
+	${BASEDIR}/sub1.sh exp_HTP threshold_R51_0.3 ${NUM_USER} 2 25 ${NUM_INSTANCE}
+	${BASEDIR}/sub1.sh exp_HTP threshold_R51_0.3 ${NUM_USER} 3 25 ${NUM_INSTANCE}
 done
